9db763668192145442771219ba0da9b64acd6af1
Robertodsantos <101083013+Robertodsantos@users.noreply.github.com>
2022-08-31 00:41:42 -0300

    Principais pastas do projeto, funcional até edição

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\SeriesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/series');
+});
+
+Route::get('/ola', function() {
+    echo 'Olá Mundo';
+});
+
+Route::resource('/series', SeriesController::class)->except(['show']);
